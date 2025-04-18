777e38f9cfe331fa74fb498fbefe9dcc936c4178
Cheron175 <hovietdanh53@gmail.com>
2025-04-18 22:58:56 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -3,3 +3,22 @@ import { sdk } from '@farcaster/frame-sdk';
 await sdk.actions.ready();
 
 await sdk.actions.ready({ disableNativeGestures: true });
+
+// gán sự kiện ngoài load
+document.addEventListener("DOMContentLoaded", () => {
+    const btn = document.getElementById("soundButton");
+    const sound = document.getElementById("mySound");
+  
+    const playSound = (e) => {
+      e.preventDefault();
+      sound.currentTime = 0;
+      sound.play().catch(console.warn);
+      // hiệu ứng feedback nhỏ
+      btn.classList.add("active");
+      setTimeout(() => btn.classList.remove("active"), 150);
+    };
+  
+    btn.addEventListener("click", playSound);
+    btn.addEventListener("touchstart", playSound);
+  });
+  
